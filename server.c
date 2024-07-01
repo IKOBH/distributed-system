e92895d631efd200c362fed54924412cd6a0b51b
Shahar Avidar <ikobh7@gmail.com>
2024-07-01 18:36:47 +0300

    core: Use exec instead of function pointer to start server & client processes..
    To further decouple processes, use execvp after building seperatly both client & server.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void run_server(){
+        printf("Server is running\n");
+}
+
+int main (int arc, int **argv){
+        run_server();
+
+        return EXIT_SUCCESS;
+}
