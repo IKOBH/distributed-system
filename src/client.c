e5daa3bfc7e1e7ba9591fd2cd6bed210b9601c08
Shahar Avidar <ikobh7@gmail.com>
2024-07-05 11:49:23 +0300

    core: Implement basic TCP client-server.
    Support single connection request, single process, single threaded communication.
    Use Fixed buffers size for both send & receive.
    Queueing up to 10 requests.
    Use localhost ip & a fixed port.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

--- src/client.c
+++ src/client.c
@@ -1,12 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void run_client(){
-        printf("Client is running\n");
+#define PORT (8080)
+#define SEND_BUFFER_BYTE_SIZE (1024)
+#define RECV_BUFFER_BYTE_SIZE (1024)
+
+void run_client()
+{
+        int client_fd;
+        struct sockaddr_in address;
+        unsigned int addrlen = sizeof(address);
+        char send_buff[SEND_BUFFER_BYTE_SIZE];
+        char recv_buff[RECV_BUFFER_BYTE_SIZE];
+        int bytes_recived = 0;
+
+        printf("Client process created\n");
+
+        if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+                perror("Failed to create socket");
+                exit(EXIT_FAILURE);
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_port = htons(PORT);
+
+        if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) <= 0)
+        {
+                perror("Invalid address/ Address not supported");
+                close(client_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        if (connect(client_fd, (struct sockaddr *)&address, addrlen) < 0)
+        {
+                perror("Failed to connect socket");
+                close(client_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        strcpy(send_buff, "Hi, I'm a client");
+
+        if (send(client_fd, send_buff, strlen(send_buff) + 1, 0) < 0)
+        {
+                perror("Failed to send data");
+                close(client_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        if ((bytes_recived = recv(client_fd, recv_buff, RECV_BUFFER_BYTE_SIZE, 0)) > 0)
+        {
+                printf("Received: %s\n", recv_buff);
+        }
+        else
+        {
+                perror("Failed to receive data");
+        }
+
+        close(client_fd);
 }
 
-int main (int argc, int **argv){
+int main(int argc, char **argv)
+{
         run_client();
 
-        return EXIT_SUCCESS;
+        exit(EXIT_SUCCESS);
 }
--- src/server.c
+++ src/server.c
@@ -1,11 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void run_server(){
-        printf("Server is running\n");
+#define PORT (8080)
+#define SEND_BUFFER_BYTE_SIZE (1024)
+#define RECV_BUFFER_BYTE_SIZE (1024)
+
+void run_server()
+{
+        int server_fd, request_fd;
+        struct sockaddr_in address;
+        int opt = 1;
+        unsigned int addrlen = sizeof(address);
+        char buffer[1024] = {0};
+        char send_buff[SEND_BUFFER_BYTE_SIZE];
+        char recv_buff[RECV_BUFFER_BYTE_SIZE];
+        int bytes_recived = 0;
+
+        printf("Server process created\n");
+
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+                perror("Failed to create socket");
+                exit(EXIT_FAILURE);
+        }
+
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+        {
+                perror("Failed to set socket options");
+                close(server_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
+
+        if (bind(server_fd, (struct sockaddr *)&address, addrlen) < 0)
+        {
+                perror("Failed to bind socket to address");
+                close(server_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 10) < 0)
+        {
+                perror("Failed to listen");
+                close(server_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        printf("Server is listening on port %d\n", PORT);
+
+        if ((request_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+                perror("Failed to accept connection");
+                close(server_fd);
+                exit(EXIT_FAILURE);
+        }
+
+        printf("Connection accepted\n");
+
+        strcpy(send_buff, "Hi client, I'm a server");
+        if ((bytes_recived = recv(request_fd, recv_buff, RECV_BUFFER_BYTE_SIZE, 0)) > 0)
+        {
+                send(request_fd, send_buff, strlen(send_buff) + 1, 0);
+                printf("Received: %s\n", recv_buff);
+        }
+        else
+        {
+                perror("Failed to receive data");
+        }
+
+        close(request_fd);
+        close(server_fd);
+
+        printf("Request arrived\n");
 }
 
-int main (int argc, int **argv){
+int main(int argc, int **argv)
+{
         run_server();
 
         return EXIT_SUCCESS;
