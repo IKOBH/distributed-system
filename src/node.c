4cb3d56787ed7cb4be7ad00698f14763aa3cbfa1
Shahar Avidar <ikobh7@gmail.com>
2024-07-10 09:08:30 +0300

    core: Support 'input' arg for client & server. Cleanup code.
    Rename structs (arg->cmd).
    Check return value using '== -1' instead '< 0'.
    Split node_args struct using new server_args_t & client_args_t structs.
    Generelize code to support more args in the future.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

--- src/node.c
+++ src/node.c
@@ -10,7 +10,7 @@ int fork_process(pid_t pid, char *const args[])
 {
         pid = fork();
 
-        if (pid < 0)
+        if (pid == -1)
         {
                 perror("Failed to fork process");
                 exit(EXIT_FAILURE);
@@ -37,10 +37,9 @@ int run(char *const args[])
 
 int main(int argc, char **argv)
 {
-
-        get_args(argc, argv);
-        run(server_arg_list);
-        run(client_arg_list);
+        get_cmds(argc, argv);
+        run(server_cmd);
+        run(client_cmd);
 
         return EXIT_SUCCESS;
 }
