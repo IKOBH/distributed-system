7fa872abcb71a3d551050feb846b154cc4b96cd9
Shahar Avidar <ikobh7@gmail.com>
2024-07-11 14:58:01 +0300

    core: pipe_utils: Add pipe utils support.
    pipe_utils current API only suuport 'handle_client_pipe'.
    
    handle_client_pipe executes code in steps & probably should be called each 'step' for every 'fd' it handles.
    
    handle_client_pipe creates a pipe between the node & client process.
    
    'dir' sets the communication direction & handles file descriptors.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

---
+++ src/pipe_utils.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "pipe_utils.h"
+
+void handle_client_pipe(process_t proc, int *fd, pipe_step_t step, pipe_direction_t dir)
+{
+        pipe_end_t child_pipe_end_to_close = dir ? E_PIPE_END_READ : E_PIPE_END_WRITE;
+        pipe_end_t child_pipe_end_to_duplicate = 1 - child_pipe_end_to_close;
+        pipe_end_t parent_pipe_end_to_close = child_pipe_end_to_duplicate;
+
+        if (proc != E_CLIENT_PROC)
+        {
+                return;
+        }
+
+        switch (step)
+        {
+        case (E_PIPE_STEP_CALL):
+                if (pipe(fd) == -1)
+                {
+                        perror("Failed to create pipe");
+                        exit(EXIT_FAILURE);
+                }
+                break;
+
+        case (E_PIPE_STEP_CHILD):
+                close(fd[child_pipe_end_to_close]);
+                if (dup2(fd[child_pipe_end_to_duplicate], STDIN_FILENO) == -1)
+                {
+                        perror("Failed to duplicate read-end of pipe to STDIN");
+                        exit(EXIT_FAILURE);
+                }
+                break;
+
+        case (E_PIPE_STEP_PARENT):
+                close(fd[parent_pipe_end_to_close]);
+                break;
+        case (E_PIPE_STEP_FORK_FAILURE):
+                close(fd[E_PIPE_END_READ]);
+                close(fd[E_PIPE_END_WRITE]);
+                break;
+        default:
+                perror("Invalid pipe step");
+                exit(EXIT_FAILURE);
+        }
+}
---
+++ src/pipe_utils.h
@@ -0,0 +1,32 @@
+#ifndef PIPE_UTILS
+#define PIPE_UTILS
+
+typedef enum
+{
+        E_SERVER_PROC,
+        E_CLIENT_PROC
+} process_t;
+
+typedef enum
+{
+        E_PIPE_STEP_CALL,
+        E_PIPE_STEP_CHILD,
+        E_PIPE_STEP_PARENT,
+        E_PIPE_STEP_FORK_FAILURE
+} pipe_step_t;
+
+typedef enum
+{
+        E_PIPE_END_READ,
+        E_PIPE_END_WRITE
+} pipe_end_t;
+
+typedef enum
+{
+        E_PIPE_DIR_PARENT_TO_CHILD, /*Parent write, child reads.*/
+        E_PIPE_DIR_CHILD_TO_PARENT  /*Child write, parent reads.*/
+} pipe_direction_t;
+
+void handle_client_pipe(process_t proc, int *fd, pipe_step_t step, pipe_direction_t dir);
+
+#endif // PIPE_UTILS
