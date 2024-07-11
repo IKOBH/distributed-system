d9379d93df3a104ca5e8a21fa5402151d3af5dda
Shahar Avidar <ikobh7@gmail.com>
2024-07-11 15:33:00 +0300

    core: Replace '#ifndef FILE_NAME' to '#pragm once' in header files.
    Use pragma once for simplicity & reduced code size.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

--- src/cmd_line_parser.h
+++ src/cmd_line_parser.h
@@ -1,9 +1,6 @@
-#ifndef CMD_LINE_PARSER
-#define CMD_LINE_PARSER
+#pragma once
 
 extern char *server_cmd[];
 extern char *client_cmd[];
 
 void get_cmds(int argc, char **argv);
-
-#endif // CMD_LINE_PARSER
