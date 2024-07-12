061fa5be21a4ef8beb73e6b268948426172f1b2c
Shahar Avidar <ikobh7@gmail.com>
2024-07-12 10:28:06 +0300

    core: Fix client-server race condition. Update node_wrapper.
    Fork both client & server before calling wait.
    add 'try_' methods to wrap pipe API.
    Delete process_t enum.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

--- src/node_wrapper.h
+++ src/node_wrapper.h
@@ -10,8 +10,23 @@
 #include "cmd_line_parser_api.h"
 #include "pipe_utils_api.h"
 
-typedef enum
+void try_handle_pipe_create(int pipe_fd[], bool use_pipe)
 {
-        E_SERVER_PROC,
-        E_CLIENT_PROC
-} process_t;
+        if (use_pipe)
+                handle_pipe_create(pipe_fd);
+}
+void try_handle_child_pipe_end(int pipe_fd[], pipe_direction_t direction, bool use_pipe)
+{
+        if (use_pipe)
+                handle_child_pipe_end(pipe_fd, direction);
+}
+void try_handle_parent_pipe_end(int pipe_fd[], pipe_direction_t direction, bool use_pipe)
+{
+        if (use_pipe)
+                handle_parent_pipe_end(pipe_fd, direction);
+}
+void try_handle_pipe_fork_failure(int pipe_fd[], bool use_pipe)
+{
+        if (use_pipe)
+                handle_pipe_fork_failure(pipe_fd);
+}
