618066c6fac660ffe41949952d704c09fa77c051
Shahar Avidar <ikobh7@gmail.com>
2024-07-15 13:14:39 +0300

    core: parser: Refactor parser. Macro & struct members renaming.
    Use a more accurate, desriptive name for user input for client & serve user args.
    
    Signed-off-by: Shahar Avidar <ikobh7@gmail.com>

--- src/cmd_line_parser.c
+++ src/cmd_line_parser.c
@@ -16,8 +16,8 @@
 
 #include "cmd_line_parser_api.h"
 
-#define SERVER_INPUT_ARG (1000)
-#define CLIENT_INPUT_ARG (1001)
+#define USER_ARGS_FOR_SERVER (1000)
+#define USER_ARGS_FOR_CLIENT (1001)
 
 #define SERVER_ARG_COUNT (3)
 #define CLIENT_ARG_COUNT (3)
@@ -28,13 +28,13 @@ char *client_cmd[CLIENT_ARG_COUNT];
 typedef struct client_args_t
 {
         char *executable;
-        char *input;
+        char *user_args;
 } client_args_t;
 
 typedef struct server_args_t
 {
         char *executable;
-        char *input;
+        char *user_args;
 } server_args_t;
 typedef struct node_args_t
 {
@@ -47,9 +47,9 @@ static struct node_args_t node_args;
 static struct option long_options[] = {
     {"help", no_argument, 0, 'h'},
     {"server_executable", required_argument, 0, 's'},
-    {"server_args", required_argument, 0, SERVER_INPUT_ARG},
+    {"server_args", required_argument, 0, USER_ARGS_FOR_SERVER},
     {"client_executable", required_argument, 0, 'c'},
-    {"client_args", required_argument, 0, CLIENT_INPUT_ARG},
+    {"client_args", required_argument, 0, USER_ARGS_FOR_CLIENT},
     {0, 0, 0, 0}};
 
 /**
@@ -63,9 +63,9 @@ static void print_usage(const char *prog_name)
         printf("\t It can run any custom client & server pair you wish for, as long as you supply the relevant executables & args.\n\n");
         printf("Usage:\t%s -s server executable [--server_args] -c client executable [--client_args] [-h]\n\n", prog_name);
         printf("\t-s server executable\tSpecify the server executable\n");
-        printf("\t--server_args server arguments\tSpecify the server arguments\n");
+        printf("\t--server_args server arguments\tSpecify the user arguments for the server\n");
         printf("\t-c client executable\tSpecify the client executable\n");
-        printf("\t--client_args client arguments\tSpecify the client arguments\n");
+        printf("\t--client_args client arguments\tSpecify the user arguments for the client\n");
         printf("\t-h\t\t\tDisplay this help message and exit\n");
 }
 
@@ -85,10 +85,10 @@ static void verify_required_args()
 static void build_cmds()
 {
         server_cmd[0] = node_args.server_args.executable;
-        server_cmd[1] = node_args.server_args.input;
+        server_cmd[1] = node_args.server_args.user_args;
         server_cmd[2] = NULL;
         client_cmd[0] = node_args.client_args.executable;
-        client_cmd[1] = node_args.client_args.input;
+        client_cmd[1] = node_args.client_args.user_args;
         client_cmd[2] = NULL;
 }
 
@@ -99,9 +99,9 @@ static void build_cmds()
 void set_default()
 {
         node_args.server_args.executable = "./server";
-        node_args.server_args.input = "";
+        node_args.server_args.user_args = "";
         node_args.client_args.executable = "./client";
-        node_args.client_args.input = "";
+        node_args.client_args.user_args = "";
 }
 /**
  * @brief    Text
@@ -124,14 +124,14 @@ static void parse_input(int argc, char **argv)
                 case 's':
                         node_args.server_args.executable = optarg;
                         break;
-                case SERVER_INPUT_ARG:
-                        node_args.server_args.input = optarg;
+                case USER_ARGS_FOR_SERVER:
+                        node_args.server_args.user_args = optarg;
                         break;
                 case 'c':
                         node_args.client_args.executable = optarg;
                         break;
-                case CLIENT_INPUT_ARG:
-                        node_args.client_args.input = optarg;
+                case USER_ARGS_FOR_CLIENT:
+                        node_args.client_args.user_args = optarg;
                         break;
                 default:
                         printf("\tFailed to find option.\n\n");
